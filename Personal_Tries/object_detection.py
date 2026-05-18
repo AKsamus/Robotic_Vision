5ff3c5e7888e93bbf902826ef34387a41d4612a4
AK <samusak47@gmail.com>
2026-05-18 11:15:42 +0200

    feat: side by side comparision for masking effect

--- Personal_Tries/object_detection.py
+++ Personal_Tries/object_detection.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 img = cv2.imread("assets/professors_ref_imgs/shapes.png")
 img_fit = cv2.resize(img, (800, 600))
 #cv2.imshow("Image", img_fit)
@@ -22,6 +23,10 @@ while True:
         """
     #simple way: overlay the mask on the original image to highlight the detected shapes in red
     result = cv2.bitwise_and(img_fit, img_fit, mask=mask)
+    # save the result image and display it along with the mask and the original image, allowing the user to press 'q' to exit the display windows
+    result_vs_original = cv2.hconcat([img_fit, result])  # Concatenate original and result images side by side
+    cv2.imshow("Result vs Original", result_vs_original)  # Display the concatenated image
+    cv2.imwrite("Results/object_detection/red_shapes.png", result_vs_original)  # Save the result image
     cv2.imshow("Image", result)
     cv2.imshow("Mask", mask)
     cv2.imshow("Original Image", img_fit)
